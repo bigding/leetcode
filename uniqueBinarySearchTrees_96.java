94d312a140b105671a6bf9ef6491299780809710
bigding <1037196370@qq.com>
2017-06-27 23:16:13 +0800

    完成121,377,96

---
+++ uniqueBinarySearchTrees_96.java
@@ -0,0 +1,30 @@
+/*******
+    ***created by bigding at 2017年06月27日 星期二 22时54分56秒
+    ***introduce:
+ *******/
+
+public class uniqueBinarySearchTrees_96{
+    public static int numTrees(int n){
+        int[] tip = new int[n + 1];
+        for(int i = 0; i <= n; i++){
+            tip[i] = -1;
+        }
+        return f(n,tip);
+    }
+    public static int f(int n, int[] tip){
+        if(n <= 1)
+            return 1;
+        if(n == 2)
+            return 2;
+        else{
+            if(tip[n] == -1){
+                int num = 0;
+                for(int i = 1; i <= n; i++){
+                    num += f(i - 1, tip) * f(n - i, tip);
+                }
+                tip[n] = num;
+            }
+            return tip[n];
+        }
+    }
+}
